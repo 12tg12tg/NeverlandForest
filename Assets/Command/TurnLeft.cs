efb391a3adcc59a012ceeb0da1df664d5e144b96
wootak1995 <85159317+wootak1995@users.noreply.github.com>
2021-12-13 12:09:02 +0900

    커맨드 패턴 undo기능까지

---
+++ Assets/Command/TurnLeft.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnLeft : Command
+{
+    private TestController m_controller;
+    public TurnLeft(TestController controller)
+    {
+        m_controller = controller;
+    }
+    public override void Execute()
+    {
+        m_controller.Turn(TestController.Direction.Left);
+    }
+    public override void Undo()
+    {
+        m_controller.Turn(TestController.Direction.RIght);
+    }
+}
+
---
+++ Assets/TestHandler.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class TestHandler : MonoBehaviour
+{
+    private Invoker invoker;
+    private bool isReplaying;
+    private bool isRecording;
+    private TestController controller;
+    private Command buttonA, buttonD , PreCommand;
+    
+    private void Start()
+    {
+        invoker = gameObject.AddComponent<Invoker>();
+        controller = FindObjectOfType<TestController>();
+
+        buttonA = new TurnLeft(controller);
+        buttonD = new TurnRight(controller);
+    }
+    private void Update()
+    {
+        if (!isReplaying && isRecording)
+        {
+            if (Input.GetKeyUp(KeyCode.A))
+            {
+                invoker.ExecuteCommand(buttonA);
+                Debug.Log("<-");
+                PreCommand = buttonA;
+            }
+
+            if (Input.GetKeyUp(KeyCode.D))
+            {
+                invoker.ExecuteCommand(buttonD);
+                Debug.Log("->");
+                PreCommand = buttonD;
+            }
+
+        }
+    }
+
+    void OnGUI()
+    {
+        if (GUILayout.Button("Start Recording"))
+        {
+            controller.ResetPosition();
+            isReplaying = false;
+            isRecording = true;
+            invoker.Record();
+        }
+        if (GUILayout.Button("Stop Recording"))
+        {
+            controller.ResetPosition();
+            isReplaying = false;
+
+        }
+        if (GUILayout.Button("Start Replay"))
+        {
+            controller.ResetPosition();
+            isRecording = false;
+            isReplaying = true;
+            invoker.Replay();
+        }
+        if (GUILayout.Button("Undo"))
+        {
+            invoker.UndoCommand(PreCommand);
+        }
+    }
+}
