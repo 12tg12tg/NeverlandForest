ac9930d3e73b557ebaea70438354666030ddaa05
inho9979 <inho9979@naver.com>
2021-12-23 11:24:23 +0900

    던전맵 세팅중

---
+++ Assets/Test/TestRandomMap/MapCreateTest/MapRoadCtrl.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapRoadCtrl : MonoBehaviour
+{
+    public GameObject SpawnPos;
+    public GameObject endPos;
+
+    public DungeonRoom curRoom;
+
+    void Start()
+    {
+        var childEnd = gameObject.GetComponentsInChildren<EndPos>();
+
+        for (int i = 0; i < childEnd.Length; i++)
+        {
+            if (i == childEnd.Length - 1)
+            {
+                childEnd[i].isLastPos = true;
+            }
+        }
+    }
+}
