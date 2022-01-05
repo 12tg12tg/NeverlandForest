70a457f7efa5f76fa8e97c47c374f78ca4946b59
steidl90 <87983461+steidl90@users.noreply.github.com>
2022-01-05 12:37:47 +0900

    월드맵 버그 수정, 던전맵 터치 이동, 미니맵 리사이즈, 토글 완료

--- Assets/Test/2ENO/Unit/Player/Control/PlayerDungeonUnit.cs
+++ Assets/Test/2ENO/Unit/Player/Control/PlayerDungeonUnit.cs
@@ -21,6 +21,7 @@ public class PlayerDungeonUnit : UnitBase
     private bool isMove;
     private PlayerMoveAnimation curAnimation;
     private bool isCoMove;
+    private MultiTouch multiTouch;
 
     private int curRoomNumber = 0;
     public bool IsCoMove
@@ -48,39 +49,26 @@ public class PlayerDungeonUnit : UnitBase
     private void Start()
     {
         playerAnimation = gameObject.GetComponent<Animator>();
+        multiTouch = GameManager.Manager.MultiTouch;
     }
 
     void Update()
     {
         // 테스트용, 던전 이동씬에서만 동작!
-        if (!isCoMove)
+        if (!isCoMove && multiTouch.TouchCount > 0)
         {
             isMove = false;
-            if (Camera.main.ScreenToViewportPoint(new Vector3(MultiTouch.Instance.PrimaryPos.x,
-                MultiTouch.Instance.PrimaryPos.y, 0f)).x > 0.5f && MultiTouch.Instance.TouchCount >= 1)
+            // 내가 터치한 지점이 플레이어보다 왼쪽인지 오른쪽인지 판단하는 형태로 구현하기..
+            var playerXPos = Camera.main.WorldToViewportPoint(transform.localPosition).x;
+            var touchXPos = Camera.main.ScreenToViewportPoint(multiTouch.PrimaryPos).x;
+            if (playerXPos < touchXPos)
             {
                 var pos = Vector3.forward * 7f * Time.deltaTime;
                 transform.position += pos;
                 transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
                 isMove = true;
             }
-            else if (Camera.main.ScreenToViewportPoint(new Vector3(MultiTouch.Instance.PrimaryPos.x,
-                MultiTouch.Instance.PrimaryPos.y, 0f)).x > 0f && MultiTouch.Instance.TouchCount >= 1)
-            {
-                var pos = -Vector3.forward * 7f * Time.deltaTime;
-                transform.position += pos;
-                transform.rotation = Quaternion.Euler(new Vector3(0f, 180f, 0f));
-                isMove = true;
-            }
-
-            if (Input.GetKey(KeyCode.D))
-            {
-                var pos = Vector3.forward * 7f * Time.deltaTime;
-                transform.position += pos;
-                transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
-                isMove = true;
-            }
-            else if (Input.GetKey(KeyCode.A))
+            else if (playerXPos > touchXPos)
             {
                 var pos = -Vector3.forward * 7f * Time.deltaTime;
                 transform.position += pos;
