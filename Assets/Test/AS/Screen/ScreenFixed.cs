fe07bef04c9b56d032516d92828c824f7bdbab14
steidl90 <87983461+steidl90@users.noreply.github.com>
2022-01-27 14:15:57 +0900

    화면 비율 코드 정리

--- Assets/Test/AS/Screen/ScreenFixed.cs
+++ Assets/Test/AS/Screen/ScreenFixed.cs
@@ -14,28 +14,32 @@ public class ScreenFixed : MonoBehaviour
 
     public void SetResolution()
     {
-        var deviceWidth = Screen.width; // 기기 너비 저장
-        var deviceHeight = Screen.height; // 기기 높이 저장
+        // 디바이스의 가로, 세로 가져오기
+        var deviceWidth = Screen.width;
+        var deviceHeight = Screen.height;
 
-        Debug.Log($"{deviceWidth} {deviceHeight}");
-
-        var ratioFixed = (float)fixedWidth / fixedHeight;
-        var deviceFixed = (float)deviceWidth / deviceHeight;
-
-        Debug.Log(deviceFixed);
+        // 내가 정한 사이즈의 비율과 사용자의 디바이스 사이즈 비율
+        var myRatioFixed = (float)fixedWidth / fixedHeight;
+        var deviceRatioFixed = (float)deviceWidth / deviceHeight;
 
         // 16:9의 비율로 변경한다
         Screen.SetResolution(fixedWidth, (int)(((float)deviceHeight / deviceWidth) * fixedWidth), true);
 
-        if (ratioFixed < deviceFixed) // 기기의 해상도 비가 더 큰 경우
+        // 가로가 더 큰 경우와 세로가 더 큰 경우에 따라 보정해주는 것(가로든 세로든 하나는 꽉 찬 상태로)
+        if (myRatioFixed < deviceRatioFixed)
         {
-            float newWidth = ratioFixed / deviceFixed; // 새로운 너비
-            Camera.main.rect = new Rect((1f - newWidth) / 2f, 0f, newWidth, 1f); // 새로운 Rect 적용
+            // 넓이 다시 계산
+            float newWidth = myRatioFixed / deviceRatioFixed;
+
+            // 카메라를 보정해주는 것으로 꽉 찬 상태로 만들어 주는 것
+            // 카메라를 보정해주지 않으면 16:9 비율은 유지하지만 가로, 세로 어디든 꽉찬 상태가 되지 않는다
+            Camera.main.rect = new Rect((1f - newWidth) / 2f, 0f, newWidth, 1f);
         }
-        else // 게임의 해상도 비가 더 큰 경우
+        else
         {
-            float newHeight = deviceFixed / ratioFixed; // 새로운 높이
-            Camera.main.rect = new Rect(0f, (1f - newHeight) / 2f, 1f, newHeight); // 새로운 Rect 적용
+            // 높이 다시 계산
+            float newHeight = deviceRatioFixed / myRatioFixed;
+            Camera.main.rect = new Rect(0f, (1f - newHeight) / 2f, 1f, newHeight);
         }
     }
 }
