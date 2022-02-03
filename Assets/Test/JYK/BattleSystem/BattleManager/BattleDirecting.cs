cd2b434497b023b89e5c69d3073e9c8f6210098e
12tg12tg <12tg@naver.com>
2022-02-03 17:24:49 +0900

    ITween 받기 전

---
+++ Assets/Test/JYK/BattleSystem/BattleManager/BattleDirecting.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleDirecting : MonoBehaviour
+{
+    private BattleManager bm;
+    private Camera battleCamera;
+
+    public void ShowMonsterSide()
+    {
+
+    }
+    
+    public void ShowBattleSide()
+    {
+
+    }
+}
