46ebd0362fb38b889842dc35493e20a49f0e226b
wootak1995 <85159317+wootak1995@users.noreply.github.com>
2022-02-11 15:43:43 +0900

    캠프씬 미니맵 적용 완료

--- Assets/Test/2ENO/DunGeonMap/Camera/MiniMapCamMove.cs
+++ Assets/Test/2ENO/DunGeonMap/Camera/MiniMapCamMove.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 public class MiniMapCamMove : MonoBehaviour
 {
     public Vector3 leftVec;
@@ -21,7 +18,7 @@ public class MiniMapCamMove : MonoBehaviour
     {
         //Init();
     }
-
+  
     public void Init()
     {
         //minimapImg.sizeDelta = new Vector2(200f, 200f);
@@ -32,46 +29,39 @@ public class MiniMapCamMove : MonoBehaviour
 
     void Update()
     {
-        if (!IsExpand)
+        if (GameManager.Manager.State == GameState.Dungeon)
         {
-            var curObj = DungeonSystem.Instance.minimapGenerate.dungeonRoomObjectList.Find(x => x.roomIdx == DungeonSystem.Instance.DungeonSystemData.curDungeonRoomData.roomIdx);
-            if (curObj != null)
+            if (!IsExpand)
             {
-                transform.position = new Vector3(curObj.gameObject.transform.position.x, 150f, curObj.gameObject.transform.position.z);
+                var curObj = DungeonSystem.Instance.minimapGenerate.dungeonRoomObjectList.Find(x => x.roomIdx == DungeonSystem.Instance.DungeonSystemData.curDungeonRoomData.roomIdx);
+                if (curObj != null)
+                {
+                    transform.position = new Vector3(curObj.gameObject.transform.position.x, 150f, curObj.gameObject.transform.position.z);
+                }
             }
-        }
-        else
-        {
-            transform.position = new Vector3((leftVec.x + rightVec.x) / 2, 150f, ((topVec.z + bottomVec.z)/ 2) - 5f);
+            else
+            {
+                transform.position = new Vector3((leftVec.x + rightVec.x) / 2, 150f, ((topVec.z + bottomVec.z) / 2) - 5f);
+            }
         }
     }
 
     public void ExpandTrue() => IsExpand = true;
 
     public void ExpandFalse() => IsExpand = false;
-
-
-    public void ExpandMinimapToggle()
+  
+    public void SetMinimapObjectInCamp()
     {
-        //var toggle = minimapImg.GetComponent<Toggle>();
-        //var rect = canvas.GetComponent<RectTransform>().rect;
-        //var camera = GetComponent<Camera>();
+        var list = CampManager.Instance.minimpaGenerate.dungeonRoomObjectList;
+        var curObj = list.Find(x => x.roomIdx == CampManager.Instance.CurDungeonRoomIndex);
+        if (curObj != null)
+        {
+            transform.position = new Vector3(curObj.gameObject.transform.position.x, 150f, curObj.gameObject.transform.position.z);
+        }
+    }
 
-        //IsExpand = !toggle.isOn;
-        //if(IsExpand)
-        //{
-        //    minimapImg.anchoredPosition = new Vector2(rect.width * 0.5f, -rect.height * 0.5f);
-        //    minimapImg.sizeDelta = new Vector2(rect.width * 0.9f, rect.height * 0.9f);
-        //    camera.fieldOfView = 120f;
-        //}
-        //else
-        //{
-        //    minimapImg.anchoredPosition = startPos;
-        //    minimapImg.sizeDelta = startSize;
-        //    camera.fieldOfView = 60f;
-        //}
+    public void SetMinimapCameraInCamp()
+    {
+        transform.position = new Vector3((leftVec.x + rightVec.x) / 2, 150f, ((topVec.z + bottomVec.z) / 2) - 5f);
     }
 }
-//minimapImg.anchoredPosition = !IsExpand ? startPos : new Vector2(rect.width * 0.5f, -rect.height * 0.5f);
-//minimapImg.sizeDelta = !IsExpand ? new Vector2(rect.width * 0.2f, rect.height * 0.3f) : new Vector2(rect.width * 0.9f, rect.height * 0.9f);
-//camera.fieldOfView = !IsExpand ? 60f : 120f;
