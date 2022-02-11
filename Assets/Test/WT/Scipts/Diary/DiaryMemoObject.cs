7e7127bf3cf62746923a52541ef019df743b2f32
wootak1995 <85159317+wootak1995@users.noreply.github.com>
2022-02-11 12:08:45 +0900

    메모테이블 수정 이걸로 쓰면됨

--- Assets/Test/WT/Scipts/Diary/DiaryMemoObject.cs
+++ Assets/Test/WT/Scipts/Diary/DiaryMemoObject.cs
@@ -4,13 +4,35 @@ using TMPro;
 public class DiaryMemoObject : MonoBehaviour
 {
     [SerializeField] private Image memoimage;
+    public Image MemoImage
+    {
+        get => memoimage;
+        set { memoimage = value; }
+    }
     [SerializeField] private TextMeshProUGUI text;
+    public TextMeshProUGUI Text
+    {
+        get => text;
+        set { text = value; }
+    }
+    private DiaryMemo memo;
+    private string memoid;
+    public string Id
+    {
+        get => memoid;
+        set { memoid = value; }
+    }
 
-    /*
-        public void Init(MemoTable table, string id, )
-        {
+    public void Init(MemoTable table, string id, DiaryMemo diaryMemo)
+    {
+        this.memo = diaryMemo;
+        memoid = id;
+        text.text = table.GetData<MemoTableElem>(id).desc;
+    }
 
-        }
-    */
-
+    public void ButtonOnClick()
+    {
+        memo.currentMemo = this;
+        memo.OnChangedSelection();
+    }
 }
