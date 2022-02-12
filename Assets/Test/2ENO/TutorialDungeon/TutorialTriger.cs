35b2ac028adac13859b8744ee2877a35ba82f814
inho9979 <inho9979@naver.com>
2022-02-12 16:50:26 +0900

    프리팹 수정(배경적용?) 튜토리얼 전용 프리팹 - 스토리 트리거용

---
+++ Assets/Test/2ENO/TutorialDungeon/TutorialTriger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialTriger : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag is "Player")
+        {
+            // 스토리 챕터 마지막!
+
+        }
+    }
+}
